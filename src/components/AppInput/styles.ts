b4c972b34ae44280ef9fa52cf25a3711bcf521af
Davi Chechetto <davichechetto@gmail.com>
2026-06-15 23:32:22 -0300

    style - mudando cor do input

--- src/components/AppInput/styles.ts
+++ src/components/AppInput/styles.ts
@@ -6,11 +6,11 @@ import { typography } from "../../styles/typography";
 export const styles = StyleSheet.create({
   input: {
     marginBottom: 22,
-    backgroundColor: colors.background,
     fontFamily: typography.fontFamily.regular,
   },
   outline: {
     borderRadius: 16,
     borderWidth: 1,
+    backgroundColor: colors.surface,
   },
 });
